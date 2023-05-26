238b2c2e78ec9c776e63d3b3cdb510a0d37d309d
gvheisler <gvheisler@inf.ufsm.br>
2023-05-26 12:31:27 -0300

    more tests made

--- data-mining/apriori.r
+++ data-mining/apriori.r
@@ -4,7 +4,7 @@ ds <- read.csv2(file.choose())
 #ds <- read.csv2(url("https://ti.saude.rs.gov.br/covid19/download"))
 
 ds <- ds[,c(13, 16:20)]
-ds <- ds[-which(ds$EVOLUCAO!='OBITO'),]
+#ds <- ds[-which(ds$EVOLUCAO!='OBITO'),]
 
 colnames(ds) <- c("DEVELOPMENT", "COUGHING", "FEVER", "SORE THROAT", "SHORTNESS OF BREATH", "OTHER")
 
@@ -12,13 +12,18 @@ for(i in 2:ncol(ds)){
   ds[,i]<-ifelse(ds[,i]=="SIM","YES","NO")
 }
 
-ds$DEVELOPMENT <- ifelse(ds$DEVELOPMENT=="OBITO","DEATH","")
+ds$DEVELOPMENT <- ifelse(ds$DEVELOPMENT=="OBITO","DEATH","RECOVERED")
 
 for (i in 1:ncol(ds)) {
   ds[,i] <- as.factor(ds[,i])
 }
 
-regras <- apriori(data = ds, parameter = list(conf = 0.05, supp = 0.05), target = 'rules', minlen = 2, maxlen = 2)
-regras <- subset(regras, rhs %in% 'DEVELOPMENT=DEATH')
-regras <- sort(regras, by = 'support', decreasing = TRUE)
-inspect(regras)
+supports <- c(1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.05,0.01,0.001);
+
+for(sup in supports){
+  regras <- apriori(data = ds, parameter = list(conf = 0.001, supp = sup), target = 'rules', minlen = 2, maxlen = 10)
+  regras <- subset(regras, rhs %in% 'DEVELOPMENT=DEATH')
+  regras <- sort(regras, by = 'confidence', decreasing = TRUE)
+  inspect(regras)
+}
+
