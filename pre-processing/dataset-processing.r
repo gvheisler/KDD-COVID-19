dfb0396c51ead58b8316ed87a32b96953da7fdd5
gvheisler <gvheisler@inf.ufsm.br>
2024-04-11 11:26:04 -0300

    apresentação pronta eu acho

--- pre-processing/dataset-processing.r
+++ pre-processing/dataset-processing.r
@@ -7,3 +7,14 @@ ds <- ds[which(ds$DATA_CONFIRMACAO<=as.Date("31/12/2022", format = "%d/%m/%Y")),
 
 write.csv2(ds, "data.csv")
 
+obitos <- nrow(ds[which(ds$EVOLUCAO=="OBITO"),])
+recuperados <- nrow(ds[which(ds$EVOLUCAO=="RECUPERADO"),])
+
+total <- obitos + recuperados
+
+p_recuperados <- (recuperados*100)/total
+p_obitos <- (obitos*100)/total
+
+p_total <- p_recuperados + p_obitos
+
+p_total
